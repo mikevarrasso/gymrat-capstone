b4d37ab5681dc5f3910ba67f041cf33855dabc3e
mikevarrasso <mikevarrasso@hotmail.com>
2022-11-18 12:58:49 -0500

    Image srcs changed

--- login.php
+++ login.php
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="gymrat.css">
-    <link rel="icon" href="public/images/Rat.png" type="image/x-icon" />
+    <link rel="icon" href="gymrat/images/Rat.png" type="image/x-icon" />
     <title>Gym Rat | Log in</title>
 </head>
 
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <!-- Page header -->
     <div id="bar">
         <div id="barTitle">Gym Rat</div>
-        <div id="logo"><img src="public/images/Rat.png" height="60px" /></div>
+        <div id="logo"><img src="gymrat/images/Rat.png" height="60px" /></div>
     </div>
 
     <div id="loginBlock">
